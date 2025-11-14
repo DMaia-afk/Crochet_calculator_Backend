742605ebc7be6ab53578458b4230426299a453a3
Diego Maia <diego.trab202@gmail.com>
2025-11-13 21:31:40 -0300

    Remove unused login path from main/urls.py

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
     path('notes/', views.ListNotesView.as_view(), name='notes'),
     path('calculate/', views.calculate, name='calculate'),
 ]
